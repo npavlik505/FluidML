d858fadb50eeeb33766d7f8c4f5552f9f4dd334e
npavlik505 <nathanielpavlik@gmail.com>
2023-11-27 17:14:24 -0800

    Lorenz data generator, h5py files

---
+++ Source/TimeSeriesData/TimeSeriesData.py
@@ -0,0 +1,86 @@
+#Create .hdf5 files of lorenz X,Y,Z
+import h5py
+import io
+import numpy as np
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class LorenzDataGenerator:
+
+    def __init__(self, sigma, rho, beta, time, dt, steps, datasets):
+        #Parameters
+        self.sigma = sigma
+        self.rho = rho
+        self.beta = beta
+        self.dt = time
+        self.dt = dt
+        self.steps = steps
+        self.datasets = datasets
+
+    def LorenzData(self, s):
+        s = s.to(torch.float32)
+        dFL = [s[0] + (self.sigma*(s[1] - s[0])),
+            s[1] + (s[0]*(self.rho - s[2]) - s[1]),
+            s[2] + (s[0]*s[1] - self.beta*s[2])]
+        #list comprehension to multiply a float by a list
+        dFL = [x*self.dt for x in dFL]
+        dFL = torch.tensor(dFL)
+        return dFL
+
+#Random Initial Conditions
+    def reset(self):
+        self.F0 = np.array([np.random.uniform(-21, 21), np.random.uniform(-16, 16), np.random.uniform(-1, 46)])
+        self.F0 = torch.tensor(self.F0)
+        return self.F0
+
+#Take one step forward in the Lorenz System
+    def step(self, s):
+        s = torch.tensor(s)
+        s += torch.tensor(self.LorenzData(s))
+        return s
+
+sigma = 10
+rho = 28
+beta = 8/3
+dt = .001
+time = 5
+steps = time/dt
+print(type(steps))
+datasets = 5
+XYZ = LorenzDataGenerator(sigma, rho, beta, time, dt, steps, datasets)
+for x in range(0,XYZ.datasets):
+    file = h5py.File('LorenzDataSet_' + str(x) + '.h5py', 'w')
+    s = XYZ.reset()
+    DataSet = []
+    DataSetX = []
+    DataSetY = []
+    DataSetZ = []
+    for i in range(int(steps)):
+        DataSet.append(s)
+        DataSetX.append(s[0])
+        DataSetY.append(s[1])
+        DataSetZ.append(s[2])
+        s = XYZ.step(s)
+        if i == (steps-1): 
+            file.create_dataset("XYZ_Data", data = DataSet)
+            file.create_dataset("X_Data", data = DataSetX)
+            file.create_dataset("Y_Data", data = DataSetY)
+            file.create_dataset("Z_Data", data = DataSetZ)
+            print('here')
+            file.close()
+
+
+#%%
+import h5py
+import numpy as np
+view = h5py.File('LorenzDataSet_0.h5py', 'r')
+view.keys()
+view1 = view.get("X_Data")
+view1 = np.array(view1)
+print(view1.shape)
+#%%
